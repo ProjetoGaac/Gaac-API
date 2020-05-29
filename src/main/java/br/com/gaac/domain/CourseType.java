99ecaa33442dcafc45ec36927bc0e5b18db263f8
Gabriel Batista <gbaalmeida@hotmail.com>
2020-05-29 18:04:47 -0300

    CourseType class Created

---
+++ src/main/java/br/com/gaac/domain/CourseType.java
@@ -0,0 +1,30 @@
+package br.com.gaac.domain;
+
+public class CourseType{
+
+    private String nome;
+    private String descricacao;
+
+    public CourseType(String nome, String descricacao){
+        this.nome = nome;
+        this.descricacao = descricacao;
+    }
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricacao() {
+		return this.descricacao;
+	}
+
+	public void setDescricacao(String descricacao) {
+		this.descricacao = descricacao;
+	}
+
+
+}
