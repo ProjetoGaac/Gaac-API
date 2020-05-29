e3a2b5e5cdc4bd1a84bf08e44298dcc85d9060cf
Felipe Duarte <felipe15lfde@gmail.com>
2020-05-29 20:07:11 -0300

    creating the generalManager class

---
+++ src/main/java/br/com/gaac/domain/GeneralManager.java
@@ -0,0 +1,14 @@
+/** Class that represents General Manager
+ * @author Felipe Duarte
+ * @version 1.0.0 */
+
+package br.com.gaac.domain;
+
+public class GeneralManager extends Employee{
+	
+	/** Default Constructor*/
+	public GeneralManager() {
+		
+	}
+	
+}
