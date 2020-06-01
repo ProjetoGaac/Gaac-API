e5edcbf577de97d406aa22b720fa44c0eee5b049
Gabriel Batista <gbaalmeida@hotmail.com>
2020-06-01 16:03:49 -0300

    Starting the teatcher class

---
+++ src/main/java/br/com/gaac/domain/teacher.java
@@ -0,0 +1,13 @@
+/** Class that represents Subject
+ * @author Gabriel Batista
+ * @version 1.0.0 */
+
+package br.com.gaac.domain;
+
+public class teacher extends Employee{
+	
+	public teacher() {
+		
+	}
+	
+}
