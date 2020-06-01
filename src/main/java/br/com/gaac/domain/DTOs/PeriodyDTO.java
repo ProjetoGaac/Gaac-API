1c8a1cf228809f2697bb11fff9e7fd6eaab2c8d0
Gabriel Batista <gbaalmeida@hotmail.com>
2020-06-01 16:55:49 -0300

    Starting the PeriodyDTO class

---
+++ src/main/java/br/com/gaac/domain/DTOs/PeriodyDTO.java
@@ -0,0 +1,22 @@
+/** Class that represents Subject
+ * @author Gabriel Batista
+ * @version 1.0.0 */
+
+package br.com.gaac.domain.DTOs;
+ 
+public class CourseDTO {
+	
+    private int ordinal;
+
+    public CourseDTO();
+
+	public int getOrdinal() {
+		return this.ordinal;
+	}
+
+	public void setOrdinal(int ordinal) {
+		this.ordinal = ordinal;
+	}
+
+    
+}
