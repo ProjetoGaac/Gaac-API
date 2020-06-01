dc28c7400bb9860859d7ded6eb9744b472b061f6
Gabriel Batista <gbaalmeida@hotmail.com>
2020-06-01 16:00:52 -0300

    uncommented

--- src/main/java/br/com/gaac/domain/CourseType.java
+++ src/main/java/br/com/gaac/domain/CourseType.java
@@ -9,31 +9,26 @@ public class CourseType{
     private String name;
     private String description;
 
-	/** Default Constructor */
+	
     public CourseType(String name, String description){
         this.name = name;
         this.description = description;
 	}
 	
-	/** Method to return the name
-	 * @return String - name */
+
 	public String getName() {
 		return this.name;
 	}
 
-	/** Method to set the name
-	 * @param String - name */
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	/** Method to get the description
-	 * @return8 String - description */
 	public String getDescription() {
 		return this.description;
 	}
-	/** Method to set the description
-	 * @param String - description */
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
