0b026fd7a108b5fa5a6736efad339dedb15733a6
Gabriel Oliveira <oliveirag661@gmail.com>
2020-06-05 00:44:00 -0300

    I made the Class CourseAdministrator and I created the single method for this class, this class created is located in the Domain Packed

---
+++ src/main/java/br/com/gaac/domain/CourseAdministrator.java
@@ -0,0 +1,8 @@
+package br.com.gaac.domain;
+
+public class CourseAdministrator {
+    //only one method this class has, I don't know the mean of this method that's why is empty
+    public CourseAdministrator(){
+
+    }
+}
