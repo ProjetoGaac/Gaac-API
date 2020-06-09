35d32f71eb951665e083caf87cc5317d627e998d
Jorge Gabriel <jorgegabriel1993@hotmail.com>
2020-06-09 04:29:10 -0300

    classes CourseTypeResource.java and SubjectResource.java added

---
+++ src/main/java/br/com/gaac/resources/CourseTypeResource.java
@@ -0,0 +1,35 @@
+/** Class that represents the resource the of CourseType class
+ * @author Jorge Gabriel
+ * @version 1.0.0 */
+
+package br.com.gaac.resources;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+
+import br.com.gaac.domain.CourseType;
+
+
+public class CourseTypeResource {
+
+    public ResponseEntity<CourseType> save(CourseType courseType){
+        return null; //implementar
+    }
+
+    public ResponseEntity<CourseType> update(CourseType courseType){
+        return null; //implementar
+    }
+
+    public ResponseEntity delete(CourseType courseType){
+        return null; //implementar
+    }
+
+    public CourseType findById(Long id){
+        return null; //implementar
+    }
+
+    public ResponseEntity<Page<CourseType>> findAll(Integer page, Integer quantityPerPage){
+        return null; //implementar
+    }
+    
+}
---
+++ src/main/java/br/com/gaac/resources/SubjectResource.java
@@ -0,0 +1,52 @@
+/** Class that represents the resource the of Subject class
+ * @author Jorge Gabriel
+ * @version 1.0.0 */
+
+package br.com.gaac.resources;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+import java.util.ArrayList;
+
+import br.com.gaac.domain.Subject;
+import br.com.gaac.domain.DTOs.SubjectDTO;
+import br.com.gaac.domain.Period;
+import br.com.gaac.domain.Teacher;
+import br.com.gaac.domain.StudentPeriod;
+
+public class SubjectResource {
+
+    public ResponseEntity<Subject> save(SubjectDTO subject){
+        return null; //implementar
+    }
+
+    public ResponseEntity<Subject> update(SubjectDTO subject){
+        return null; //implementar
+    }
+
+    public ResponseEntity delete(Subject subject){
+        return null; //implementar
+    }
+
+    public Subject findById(Long id){
+        return null; //implementar
+    }
+
+    public ArrayList<Subject> findSubjectsByPeriod(Long idPeriod){
+        return null; //implementar
+    }
+
+    public Page<Subject> findSubjectsByTeacher(Long idTeacher, Integer page, Integer quantityPerPage){
+        return null; //implementar
+    }
+
+    public ArrayList<Subject> findSubjectsByStudentPeriod(Long idStudentPeriod){
+        return null; //implementar
+    }
+
+    public ResponseEntity<Page<Subject>> findAll(Integer page, Integer quantityPerPage){
+        return null; //implementar
+    }
+    
+}
