90e7c54e1a6c2d628a96ec241218cdd5749bc639
Gabriel Oliveira <oliveirag661@gmail.com>
2020-06-12 00:40:24 -0300

    ErrorModel I created this class based in the  diagram created by Luiz

---
+++ src/main/java/br/com/gaac/resources/exceptions/ErrorModel.java
@@ -0,0 +1,36 @@
+package br.com.gaac.resources.exceptions;
+
+public class ErrorModel {
+    //Atributos
+    Integer code;
+    String status;
+    String message;
+
+    public void ErrorModel(Integer code, String status,String message){
+
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
